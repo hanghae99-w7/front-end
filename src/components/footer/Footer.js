708eb745ca73a4e7b490eab3c436b98fa6b84ee2
lucy-pill <schmidtiana95@gmail.com>
2022-08-19 20:19:30 +0900

    fix mediaquery

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,20 @@
-const Footer = () => {
+import styled from 'styled-components';
+import { useMediaQuery } from 'react-responsive';
 
-}
+const Footer = () => {
+  const isSmallScreen = useMediaQuery({
+    query: '(max-width: 1024px)',
+  });
+  return <FooterBox>{isSmallScreen ? '작은거' : '큰거'}</FooterBox>;
+};
 
 export default Footer;
+
+export const FooterBox = styled.div`
+  text-align: center;
+  font-size: 30px;
+  background-color: black;
+  color: white;
+  width: 100%;
+  height: 50px;
+`;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,20 @@
-const Header = () => {
+import styled from 'styled-components';
+import { useMediaQuery } from 'react-responsive';
 
-}
+const Header = () => {
+  const isSmallScreen = useMediaQuery({
+    query: '(max-width: 1024px)',
+  });
+  return <HeaderBox>{isSmallScreen ? '작은거' : '큰거'}</HeaderBox>;
+};
 
 export default Header;
+
+export const HeaderBox = styled.div`
+  text-align: center;
+  font-size: 30px;
+  background-color: black;
+  color: white;
+  width: 100%;
+  height: 50px;
+`;
