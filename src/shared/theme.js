a5d40f807362ff911f774c8f94cbaa79d9b7979d
lucy-pill <schmidtiana95@gmail.com>
2022-08-20 21:33:36 +0900

    finish header

--- src/shared/theme.js
+++ src/shared/theme.js
@@ -2,6 +2,7 @@ export const theme = {
   primaryColor: "#000000",
   secondaryColor: "#343434",
   tertiaryColor: "#858585",
+  quaternaryColor: "#525252",
   primaryFontSize: '13px',
   secondaryFontSize: '11px',
 }
