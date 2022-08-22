c2db36cc3fd8b4a867974ca57792662d0395a4d8
4775614 <4775614@naver.com>
2022-08-22 20:43:21 +0900

    feat:문의페이지 뷰 잡는 중

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 
 // Components & Elements import
 import Landing from './pages/landing/Landing';
+import Service from './pages/service/Service';
+
 
 function App() {
   return (
     <Routes>
       <Route path={'/'} exact={true} element={<Landing />} />
+      <Route path={'/service'} exact={true} element={<Service />} />
     </Routes>
   );
 }
