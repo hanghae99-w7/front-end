91ff43a6e8dc79981fedc33e43287900dec15b88
lucy-pill <schmidtiana95@gmail.com>
2022-08-24 02:30:18 +0900

    test

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,8 +1,9 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import user from './modules/user'
 import item from './modules/item';
+import basket from './modules/basket';
 
-const rootReducer = combineReducers({ item, user });
+const rootReducer = combineReducers({ item, user, basket });
 
 const store = configureStore({ reducer: rootReducer });
 
